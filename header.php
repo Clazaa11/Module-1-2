7608066036c6cfb318bb991381b7cce7b6b3af46
Clarence Mendoza <clarencemendoza97@gmail.com>
2025-11-29 14:13:21 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+<header id="header">
+    <h1>PC PARTS</h1>
+    <nav id="nav-bar">
+        <a href="#items" class="nav-link">Items</a>
+        <a href="#prices" class="nav-link">Pricing</a>
+    </nav>
+</header>
