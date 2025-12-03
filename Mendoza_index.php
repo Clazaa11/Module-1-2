a670f26396c0d182a959b80110d22bacc5b493f9
Clarence Mendoza <clarencemendoza97@gmail.com>
2025-12-03 18:48:05 +0800

    Update and rename Mendoza.php to Mendoza_index.php

--- Mendoza_index.php
+++ Mendoza_index.php
@@ -7,23 +7,14 @@
         "Mouse" => 1000,
         "Keyboard" => 1200,
         "Headset" => 600,
-        "Monitor" => 7500
+        "Monitor" => 7500,
+        "GPU (RTX 4060)" => 21000, //Added 4 items
+        "CPU (Ryzen 7" => 14500,
+        "SSD 1TB" => 4200,
+        "Power Supply" => 3500,
     ];
 
-    //Comment out the changes
-	// $item1 = "Mouse";
-	// $prices1 = 1000;
-
-	// $item2 = "Keyboard";
-	// $prices2 = 1200;
-
-	// $item3 = "Headset";
-	// $prices3 = 600;
-
-	// $item4 = "Monitor";
-	// $prices4 = 7500;
-
-	$bundle = array_sum($items) * 0.80;
+	$bundle = array_sum($items) * 0.85;
 ?>
 
 <!DOCTYPE html>
@@ -31,7 +22,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>GAMERS GEAR SHOP</title>
 	<link rel="stylesheet" href="storestyles.css">
 </head>
 <body>
@@ -40,11 +31,16 @@
     <section id="items">
         <h1>Available Items</h1>
         
+        <!-- Pictures of items -->
         <div class="pic-grid">
             <img src="https://m.media-amazon.com/images/I/71+p3Hx03dL._AC_SL1500_.jpg" class="item-pic">
             <img src="https://platform.theverge.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/25522631/G309_4.jpg?quality=90&strip=all&crop=7.8125,0,84.375,100" class="item-pic">
             <img src="https://dlcdnwebimgs.asus.com/gain/4BB18AEF-347E-4DB6-B78C-C0FFE1F20385/w750/h470/fwebp" class="item-pic">
             <img src="https://sm.pcmag.com/pcmag_uk/guide/t/the-best-c/the-best-curved-monitors-for-2025_5dq6.png" class="item-pic">
+            <img src="https://netcodex.ph/wp-content/uploads/2024/08/GIGABYTE-GeForce-RTX-4060-Gaming-OC-8G-Graphics-Card.jpg.webp" class="item-pic">
+            <img src="https://www.notebookcheck.net/fileadmin/Notebooks/AMD/ryzen7_badge.jpg" class="item-pic">
+            <img src="https://cf.shopee.ph/file/ph-11134207-81zte-mh8yer527imnc1" class="item-pic">
+            <img src="https://pcx.com.ph/cdn/shop/files/PS-HUNTKEY-750W-MVP-K750-80_-GOLD-FULL-MOD-1.jpg?v=1699923208" class="item-pic">
         </div>
     </section>
     
@@ -60,15 +56,7 @@
             <?php foreach ($items as $item => $price): ?>
                 <tr>
                     <td><?php echo $item; ?></td>
-                    <td>
-                        <?php 
-                            if ($price > 5000) {
-                                echo "<b>$price</b>";
-                            } else {
-                                echo $price;
-                            }
-                        ?>
-                    </td>
+                    <td><?php echo $price; ?></td>
                 </tr>
             <?php endforeach; ?>
 
